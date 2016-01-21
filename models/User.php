38c8101c2f0fc178e9f634e3948ddd478d39a2b9
NickMaster <master-7@inbox.ru>
2016-01-21 21:28:40 +0300

    Create model user

--- models/User.php
+++ models/User.php
@@ -1,103 +1,176 @@
 <?php
-
 namespace app\models;
-
-class User extends \yii\base\Object implements \yii\web\IdentityInterface
+use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+/**
+ * User model
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $surname
+ * @property string $name
+ * @property string $password write-only password
+ * @property string $salt
+ * @property string $auth_key
+ * @property string $access_token
+ * @property string $create_date
+ *
+ */
+class User extends ActiveRecord implements IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
+    /**
+     * Validate constant
+     */
+    const MIN_LENGTH_PASS = 6;
     /**
      * @inheritdoc
      */
-    public static function findIdentity($id)
+    public static function tableName ()
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return 'blg_user';
     }
-
     /**
      * @inheritdoc
      */
-    public static function findIdentityByAccessToken($token, $type = null)
+    public function rules ()
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
+        return [
+            [['username', 'name', 'surname', 'password'], 'required'],
+            ['password', 'string', 'min' => self::MIN_LENGTH_PASS],
+            ['username', 'email'],
+            [['username', 'auth_key', 'access_token'], 'unique'],
+        ];
+    }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels ()
+    {
+        return [
+            'id' => _('ID'),
+            'name' => _('Имя'),
+            'surname' => _('Фамилия'),
+            'password' => _('Пароль'),
+            'salt' => _('Соль'),
+            'access_token' => _('Ключ авторизации'),
+        ];
+    }
+    /**
+     * Before save event handler
+     * @param bool $insert
+     * @return bool
+     */
+    public function beforeSave ($insert)
+    {
+        if (parent::beforeSave($insert))
+        {
+            if ($this->getIsNewRecord() && !empty($this->password))
+            {
+                $this->salt = $this->saltGenerator();
             }
+            if (!empty($this->password))
+            {
+                $this->password = $this->passWithSalt($this->password, $this->salt);
+            }
+            else
+            {
+                unset($this->password);
+            }
+            return true;
         }
-
-        return null;
+        else
+        {
+            return false;
+        }
     }
-
+    /**
+     * Generate the salt
+     * @return string
+     */
+    public function saltGenerator ()
+    {
+        return hash("sha512", uniqid('salt_', true));
+    }
+    /**
+     * Return pass with the salt
+     * @param $password
+     * @param $salt
+     * @return string
+     */
+    public function passWithSalt ($password, $salt)
+    {
+        return hash("sha512", $password . $salt);
+    }
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity ($id)
+    {
+        return static::findOne(['id' => $id]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken ($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
     /**
      * Finds user by username
      *
-     * @param  string      $username
-     * @return User
+     * @param string $username
+     * @return static|null
      */
-    public static function findByUsername($username)
+    public static function findByUsername ($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::findOne(['username' => $username]);
     }
-
     /**
      * @inheritdoc
      */
-    public function getId()
+    public function getId ()
     {
         return $this->id;
     }
-
     /**
      * @inheritdoc
      */
-    public function getAuthKey()
+    public function getAuthKey ()
     {
-        return $this->authKey;
+        return $this->auth_key;
     }
-
     /**
      * @inheritdoc
      */
-    public function validateAuthKey($authKey)
+    public function validateAuthKey ($authKey)
     {
-        return $this->authKey === $authKey;
+        return $this->getAuthKey() === $authKey;
     }
-
     /**
      * Validates password
      *
-     * @param  string  $password password to validate
+     * @param  string $password password to validate
      * @return boolean if password provided is valid for current user
      */
-    public function validatePassword($password)
+    public function validatePassword ($password)
     {
-        return $this->password === $password;
+        return $this->password === $this->passWithSalt($password, $this->salt);
+    }
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword ($password)
+    {
+        $this->password = $this->passWithSalt($password, $this->saltGenerator());
+    }
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey ()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
     }
 }
