4d9a3f8b33f7614bde8ce92b7a9b8eb8d52544a4
NickMasters <master-7@inbox.ru>
2016-02-09 18:26:47 +0300

    Create new command controller

---
+++ commands/CronController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+/**
+ * Class CronController
+ * @package app\commands
+ */
+class CronController extends Controller
+{
+    public function actionClearlh()
+    {
+
+    }
+}
